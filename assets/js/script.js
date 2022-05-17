0cbff51e5638d8c2e315b979832938149eebd2eb
LarisaLG <lgalisheva@gmail.com>
2022-05-17 10:49:35 +0000

    Set up stucture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+functioncheckAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+functionincrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
